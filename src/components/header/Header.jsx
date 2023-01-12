db274cc42b14e1f3d7ee79704a9bb4dbe6602fed
Wilfred <wilfred05777@gmail.com>
2023-01-12 12:41:53 +0800

    0:14:17 Fontawesome Icon

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { faBed } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="headerList">
+        <div className="headerListItem">
+          <FontAwesomeIcon icon={faBed} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
